27a2548ab6f60ead13930d7957c77d2caf24bfc6
Tehmoor23 <120462454+Tehmoor23@users.noreply.github.com>
2026-02-28 01:03:19 +0100

    Update App.js

--- App.js
+++ App.js
@@ -33,8 +33,8 @@ const GOAL_PRESETS = [33, 99, 100, 1000];
 const CITY = 'Bait-Us-Sabuh';
 const APP_LOGO_LIGHT = require('./assets/Icon3.png');
 const APP_LOGO_DARK = require('./assets/Icon5.png');
-const FORCE_TIME = null;
-// const FORCE_TIME = '19:31'; // development override, set null for real time
+// const FORCE_TIME = null;
+const FORCE_TIME = '19:31'; // development override, set null for real time
 const TERMINAL_LOCATIONS = [
   'Baitus Sabuh Nord',
   'Baitus Sabuh Süd',
