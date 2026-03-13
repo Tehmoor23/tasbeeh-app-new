d89057b8e89a125f28f52ef1a1e272a438310c2c
Tehmoor23 <120462454+Tehmoor23@users.noreply.github.com>
2026-03-13 16:54:25 +0100

    Update App.js

--- App.js
+++ App.js
@@ -5122,7 +5122,7 @@ function AppContent() {
       ) : null}
 
       <View style={styles.appMetaWrap}>
-        <Text style={[styles.appMetaVersion, { color: theme.muted }]}>Version 1.0.9.1</Text>
+        <Text style={[styles.appMetaVersion, { color: theme.muted }]}>Version 1.1.0</Text>
         <Text style={[styles.appMetaCopyright, { color: theme.muted }]}>© 2026 Tehmoor Bhatti. All rights reserved.</Text>
       </View>
     </ScrollView>
